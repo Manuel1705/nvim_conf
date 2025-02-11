811f6a9ddb6daafc21ec3d7cd95af17f7cb21295
manuel <manuelmignogna5@gmail.com>
2025-02-11 16:41:34 +0100

    commit

--- lua/manuel/init.lua
+++ lua/manuel/init.lua
@@ -1,3 +1,4 @@
 require("manuel.remap")
 require("manuel.packer")
+require("manuel.set")
 vim.cmd.colorscheme("gruber-darker")
---
+++ lua/manuel/set.lua
@@ -0,0 +1,3 @@
+vim.opt.nu = true
+vim.opt.tabstop = 4
+vim.opt.scrolloff = 8
